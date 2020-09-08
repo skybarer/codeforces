c31af82996808267fde470910677a936965ade6d
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-09-08 22:39:16 +0530

    elephant updated

--- A_Elephant.cpp
+++ A_Elephant.cpp
@@ -4,33 +4,14 @@ using namespace std;
 int calcSteps(int inp)
 {
     int steps = 0;
-
-    if (inp >= 5)
+    for (int i = 5; i > 0; i--)
     {
-        steps += inp / 5;
-        inp = inp % 5;
+        if (inp >= i)
+        {
+            steps += inp / i;
+            inp = inp % i;
+        }
     }
-    if (inp >= 4)
-    {
-        steps += inp / 4;
-        inp = inp % 4;
-    }
-    if (inp >= 3)
-    {
-        steps += inp / 3;
-        inp = inp % 3;
-    }
-    if (inp >= 2)
-    {
-        steps += inp / 2;
-        inp = inp % 2;
-    }
-    if (inp >= 1)
-    {
-        steps += inp / 1;
-        inp = inp % 1;
-    }
-
     return steps;
 }
 
