1014f3a5f337176f87c11fb895eaf64de42df810
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-09-09 15:02:31 +0530

    queue at school

---
+++ B_Queue_at_the_School.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, t;
+    string s;
+    cin >> n >> t >> s;
+
+    for (int i = 0; i < t; i++)
+    {
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (s[j] == 'B' && s[j + 1] == 'G')
+            {
+                swap(s[j], s[j + 1]);
+                j++;
+            }
+        }
+    }
+
+    cout << s;
+}
