a12eb5dea71b14da8c94c28fdf86df1e28330d83
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-09-20 10:06:24 +0530

    remove smallest

--- A_Remove_Smallest.cpp
+++ A_Remove_Smallest.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
-#include <set>
+#include <algorithm>
 using namespace std;
 
-bool isValid(set<int> arrSet)
+bool isValid(int arr[], int n)
 {
 
-    if (arrSet.size() == 1 || arrSet.size() == 2)
-        return 1;
+    int counter = 0;
 
-    return 0;
+    if (n == 1)
+        return true;
+
+    else
+    {
+        sort(arr, arr + n);
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (arr[i + 1] - arr[i] <= 1)
+            {
+                arr[i] = -1;
+            }
+        }
+
+        // for (int i = 0; i < n; i++)
+        // {
+        //     cout << arr[i] << " ";
+        // }
+        // cout << "\n";
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] >= 0)
+                counter++;
+        }
+    }
+
+    return counter == 1;
 }
 
 int main()
@@ -21,18 +48,16 @@ int main()
         int n;
         cin >> n;
 
-        // int arr[n] = {0};
+        int arr[n] = {0};
 
-        set<int> arrSet;
         for (int j = 0; j < n; j++)
         {
             int ele;
             cin >> ele;
-            // arr[i] = ele;
-            arrSet.insert(ele);
+            arr[j] = ele;
         }
 
-        if (isValid(arrSet))
+        if (isValid(arr, n))
             cout << "YES" << endl;
         else
             cout << "NO" << endl;
