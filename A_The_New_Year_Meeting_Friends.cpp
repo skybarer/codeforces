d6c52fa25b37b087b71822e3415b0694f2dd066b
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-09-20 11:21:30 +0530

    a new year meeting friends

---
+++ A_The_New_Year_Meeting_Friends.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int arr[3] = {a, b, c};
+
+    sort(arr, arr + 3);
+
+    // for (auto ele : arr)
+    // {
+    //     cout << ele << " ";
+    // }
+
+    cout << (arr[1] - arr[0]) + (arr[2] - arr[1]);
+}
