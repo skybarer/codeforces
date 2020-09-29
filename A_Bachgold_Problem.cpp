9381b7762eeff67be64cf927a3cc96addda82957
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-09-29 19:31:29 +0530

    a batch gold problem copied

---
+++ A_Bachgold_Problem.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << n / 2 << endl;
+
+    if (n % 2 == 0)
+    {
+        for (int i = 0; i < n / 2; i++)
+        {
+            cout << "2"
+                 << " ";
+        }
+    }
+    else
+    {
+        for (int i = 0; i < (n / 2) - 1; i++)
+        {
+            cout << "2"
+                 << " ";
+        }
+        cout << "3"
+             << " ";
+    }
+}
