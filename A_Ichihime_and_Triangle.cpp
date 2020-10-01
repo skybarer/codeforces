c341829e73d4ba55581bf5d9d83d141ee0f02cd1
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-01 20:39:38 +0530

    a ichihime and triangle added

---
+++ A_Ichihime_and_Triangle.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases;
+    cin >> testCases;
+
+    while (testCases--) {
+        long long a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        cout << b << " " << c << " " << c << endl;
+    }
+}
