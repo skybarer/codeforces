18eda50dbb632cfd8d34eaee893b906941c50e72
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-04 12:33:42 +0530

    a mohmoud and ehab added

---
+++ A_Mahmoud_and_Ehab_and_the_even_odd_game.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long n;
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << "Mahmoud" << endl;
+    else
+        cout << "Ehab" << endl;
+}
