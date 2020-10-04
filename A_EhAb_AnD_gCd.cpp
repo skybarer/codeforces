f32b89dd59f7163a0a65daadc3c96a7177304a1b
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-04 13:54:01 +0530

    a ehab and gcd added

---
+++ A_EhAb_AnD_gCd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases;
+    cin >> testCases;
+
+    while (testCases--) {
+        long n;
+        cin >> n;
+        cout << 1 << " " << n - 1 << endl;
+    }
+}
