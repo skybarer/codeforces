df4c22c63a0801572498cdba04032c0e2711e13c
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-11 17:02:29 +0530

    a twoo rectangular polygon added

---
+++ A_Two_Regular_Polygons.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+using namespace std;
+#define endl "\n"
+
+void fastIo() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+void solve() {
+    long a, b;
+    cin >> a >> b;
+
+    if (a % b == 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int main() {
+    //  for fast io operations
+    fastIo();
+
+    int testCases;
+    cin >> testCases;
+
+    while (testCases--) {
+        solve();
+    }
+}
