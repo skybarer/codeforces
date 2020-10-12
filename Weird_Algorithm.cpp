16b75eff4ebe7fdd154cf2b9b605f6a0c34c9cf3
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-12 14:01:25 +0530

    weird algo added

---
+++ Weird_Algorithm.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#define endl "\n"
+
+int main() {
+    long n;
+    cin >> n;
+
+    cout << n;
+
+    while (n > 1) {
+        if (n % 2 == 0)
+            n = n / 2;
+        else
+            n = 3 * n + 1;
+        cout << " " << n;
+    }
+
+    return 0;
+}
