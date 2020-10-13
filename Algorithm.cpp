72ac44c0466627d96e43aa790665bfaec316b889
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-13 17:05:44 +0530

    algorithms added

---
+++ Algorithm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#define endl "\n"
+#define ll long long
+
+int main() {
+  long n;
+
+  cin >> n;
+  cout << n;
+
+  while (n > 1) {
+    if (n % 2 == 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
+    }
+    cout << " " << n;
+  }
+
+  return 0;
+}
