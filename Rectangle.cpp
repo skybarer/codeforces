4fdb24ad6eec2c71cda592679b24f614865f1499
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-19 19:54:27 +0530

    rectange added

---
+++ Rectangle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define endl "\n"
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a * b << " " << 2 * (a + b) << endl;
+}
