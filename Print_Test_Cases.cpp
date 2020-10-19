d56204fcbb955e84476b29d0ec125fbe9ab1b72a
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-19 20:31:41 +0530

    print test cases added

---
+++ Print_Test_Cases.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define endl "\n"
+
+int main() {
+
+    int i = 0;
+    while(cin) {
+        int x; cin >> x; 
+        if(x != 0) cout << "Case " << ++i << ": " << x << endl;
+    }
+}
