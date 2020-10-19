a0e31dc8da8794176d0a074c339877dd1f961f3c
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-19 19:44:23 +0530

    x cubic added

---
+++ X_Cubic.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define endl "\n"
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n * n * n << endl;
+}
