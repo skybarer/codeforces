47069607f72cf5a32838f388e233cd357e238195
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-19 20:01:41 +0530

    small large equal added

---
+++ Small_Large_or_Equal.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define endl "\n"
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if(a == b) cout << "a == b" << endl;
+    else if(a < b) cout << "a < b" << endl;
+    else if(a > b) cout << "a > b" << endl;
+}
