0dc0d8ff3a89b4663a134ecd31cc57c8ed6f4055
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-19 21:24:54 +0530

    circle added

---
+++ Circle.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define endl "\n"
+#define pi 3.1415926535897932384626433832795
+
+double r;
+
+int main() {
+    cin >> r;
+
+    cout << fixed << setprecision(6) <<  pi * r * r << " ";    
+    cout << fixed << setprecision(6) << 2 * pi * r << endl;    
+}
