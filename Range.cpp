df43d6f622fd1caaafaebc9810f801049877cb5d
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-19 20:04:53 +0530

    range added

---
+++ Range.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define endl "\n"
+
+int main() {
+    int a , b, c;
+    cin >> a >> b >> c;
+
+    if(a < b && b < c) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
