e18f52b1d41980d89f20dcd0a6d4ae37eaba4e13
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-19 23:30:39 +0530

    print a ches board added

---
+++ Print_a_Chessboard.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define endl "\n"
+
+int H, W;
+
+int main() {
+    while(true) {
+        cin >> H >> W;
+        if( H == 0 && W == 0) break;
+        for (int i = 0; i < H; i++) {
+         for (int j = 0; j < W; j++) {
+             if(i % 2 == 0) {
+                if(j % 2 == 0) cout << "#";
+                else cout << ".";
+             } else {
+                if(j % 2 == 0) cout << ".";
+                else cout << "#";
+             }
+            
+          }
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
