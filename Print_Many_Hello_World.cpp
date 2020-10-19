f8cc9c8a4b9c7229bf4e13e05ba103ac085c381b
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-19 20:24:11 +0530

    print many hello world

---
+++ Print_Many_Hello_World.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define endl "\n"
+
+int main() {
+    int i = 1e3;
+    while(i > 0) { 
+        cout << "Hello World" << endl; 
+        i--;
+    }
+}
