bfd8070faf741abde2c6d15421ea1c8e5abf6224
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-21 12:50:52 +0530

    spoj factorial added

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,16 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#define endl "\n"
 #define ll long long
+#define fi first
+#define se second
+#define pi 3.1415926535897932384626433832795
+#define endl "\n"
 
-ll n;
-int count;
 
-main() {
-  cin >> n;
-  for (int i = 5; n / i >= 1; i *= 5)
-    count += n / i;
-  cout << count << endl;
+void solve() {
+    ll n;
+    cin >> n;
+    int ans = 0;
+    // for (int i = 5; (n / i) > 0; i += 5) {
+    //     int num = i;
+    //     while(num % 5 == 0) {
+    //       num /= 5;  
+    //       ans++;
+    //     }
+    // }   
+    while(n >= 5) {
+        ans += n/5;
+        n /= 5;
+    }
+    cout << ans << endl;
+}
 
-  return 0;
+int main() {
+    ll testCases; cin >> testCases;
+    while (testCases--) { solve(); }
 }
