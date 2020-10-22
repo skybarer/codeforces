6b54efa0e671f8126d533e21591c2a287b78ead2
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-22 09:13:54 +0530

    a erasing seros added

--- A_Erasing_Zeroes.cpp
+++ A_Erasing_Zeroes.cpp
@@ -1,55 +1,67 @@
 #include <bits/stdc++.h>
-
-#include <iostream>
 using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mod (int)1e9+7
+#define eps (double)1e-9
+#define INF 1011111111
+#define pi  3.1415926535897932384626433832795
+#define sz(a)   ((int)(a.size()))
+#define all(a)  a.begin(), a.end()
+#define cin1(x)   cin >> (x)
+#define cin2(x,y)   cin >> (x) >> (y)
+#define cin3(x,y,z)   cin >> (x) >> (y) >> (z)
+#define cout1(x)   cout << (x) << endl
+#define cout2(x,y) cout << (x) <<" "<< (y) << endl
+#define cout3(x,y,z) cout << (x) <<" "<< (y) <<" "<< (z) << endl
+#define UNIQ(a) a.erase(unique(all(a)), a.end())
+#define FREIN(FILE) freopen(FILE,"rt",stdin)
+#define FREOUT(FILE) freopen(FILE,"wt",stdout)
 #define endl "\n"
 
-void fastIo() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-}
+template <typename T> T sqr(T a) {return (a) * (a);}
+template <typename T> T dist(T x1, T y1, T x2, T y2) {return sqrt(sqr(x1-x2) + sqr(y1-y2));}
+template <typename T> T angle(T x1, T y1, T x2, T y2){ return atan2(double(y1-y2) , double(x1-x2));}
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned char uchar;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+
+string s;
 
 void solve() {
-    string s;
-    cin >> s;
-
+    cin1(s);
     int l = 0;
     int r = 0;
-
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '1') {
-            l = i;
-            break;
-        }
+    int n = s.size();
+    
+    for (int i = 0; i < n; i++) {
+        if(s[i] == '1') l = i; break;
+    
+    } 
+    for (int i = n-1; i > 0; i--) {
+        if(s[i] == '1') r = i; break;
     }
 
-    for (int i = s.length(); i >= 0; i--) {
-        if (s[i] == '1') {
-            r = i;
-            break;
-        }
-    }
+    int ans = 0;
+    for (int i = l+1; i < r-1; i++) {
+        if(s[i] == '0') ans++;
+    } 
 
-    int count = 0;
+    cout1(ans);
 
-    for (int i = l + 1; i < r; i++) {
-        if (s[i] == '0') {
-            count++;
-        }
-    }
-
-    cout << count << endl;
 }
 
 int main() {
-    //  for fast io operations
-    fastIo();
-
-    int testCases;
-    cin >> testCases;
-
-    while (testCases--) {
-        solve();
-    }
+    ll testCases; cin >> testCases;
+    while (testCases--) { solve(); }
 }
