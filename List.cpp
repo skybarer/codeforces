507ca31fe1350d2e51f1bdc0a1f4ca5ed160b41d
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-27 20:30:42 +0530

    list added

--- List.cpp
+++ List.cpp
@@ -1,23 +1,37 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#define endl "\n"
-#define ll long long
 
 int main() {
+    int q; cin >> q;
+    list<int> l;
+    list<int>::iterator it;
+    list<int>::iterator it2;
+     it = l.end();
 
-  long n;
-  cin >> n;
+    int opr; 
+    int d, x;   
+    for (int i = 0; i < q; i++) {
+        cin >> opr;
+        switch(opr) {
+            case 0:
+                cin >> x;
+                l.insert(it, x);
+                it--;
+                break;
+            case 1: 
+                cin >> d;
+                if(d > 0) for (int i = 0; i < d; i++) it++;
+                else if(d < 0) for (int i = 0; i < ((-1) * d); i++) it--;
+                break;
+            case 2: 
+                it2 = it;
+                it2++;
+                l.erase(it);
+                it = it2;
+                break;
+        }
+    }
 
-  if (n == 1)
-    cout << 1 << endl;
-  else if (n == 2 || n == 3)
-    cout << "NO SOLUTION" << endl;
-  else {
-    for (int i = 2; i <= n; i += 2)
-      cout << i << " ";
-    for (int i = 1; i <= n; i += 2)
-      cout << i << " ";
-  }
+    for(it = l.begin(); it != l.end(); it++) cout<< *it <<endl;
 
-  return 0;
 }
