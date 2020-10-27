416d9fe5018b410e6c731f6ceb7aaaa4b2cc54fc
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-27 21:07:25 +0530

    priority queue added

---
+++ Priority_Queue.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, q; cin >> n >> q;
+    priority_queue<int> qu[n];
+    int op, t, x;
+    for (int i = 0; i < q; i++) {
+        cin >> op;
+        switch(op) {
+            case 0: 
+                cin >> t >> x;
+                qu[t].push(x);
+                break;
+            case 1: 
+                cin >> t;
+                if(qu[t].size() != 0)
+                cout << qu[t].top() << endl;
+                break;
+            case 2: 
+                cin >> t;
+                if(qu[t].size() != 0)
+                qu[t].pop();
+                break;
+        }
+    } 
+}
