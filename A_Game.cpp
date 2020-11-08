329f838ee17bcd9f9a11673b348d10f0ea4c5a5e
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-11-08 15:06:43 +0530

    A. Game addded

---
+++ A_Game.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n1, n2, k1, k2; cin >> n1 >> n2 >> k1 >> k2;
+
+    if(n1 <= n2)  cout << "Second" << endl;
+    else if(n1 > n2)  cout << "First" << endl;
+    
+}
