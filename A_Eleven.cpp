e6b52e8a2d136859acc69bb067a0d2ff00180245
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-11-14 19:40:45 +0530

    A. Eleven modified

--- A_Eleven.cpp
+++ A_Eleven.cpp
@@ -1,28 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void fibonacci();
-
-set<int> fibb;
-void fibonacci() {
-    fibb.insert(1);
-    int f0 = 1, f1 = 1, f2 = 2;
-    for (int i = 0; i < 10; i++) {
-        f2 = f0 + f1;
-        fibb.insert(f2);
-        int temp = f1;
-        f1 = f2;
-        f0 = temp;
-    } 
-    // for(auto i : fibb) cout << i <<  " "; cout << endl; 
-}
-
+typedef long long ll;
 int main() {
-    int n; cin >> n;
-    fibonacci();
+    ll n; cin >> n;
+    ll f0 = 0, f1 = 1, f2 = 0; 
     string ans;
     for (int i = 1; i <= n; i++) {
-        if(fibb.count(i) > 0) ans += 'O';
+        f2 = f0 + f1;
+        if(f2 == i) {
+            ans += 'O';
+            int temp = f1;
+            f1 = f2;
+            f0 = temp; 
+        }
         else ans += 'o';
     }
     cout << ans << endl;
