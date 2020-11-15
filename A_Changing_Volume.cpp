3346dddbf7b35d5aed16fd9884382c7cad34ca61
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-11-15 11:16:56 +0530

    A. Changing Volume added

---
+++ A_Changing_Volume.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc; cin >> tc;
+    while(tc--) {
+        int a, b; cin >> a >> b;
+        if(a == b) cout << 0 << endl;
+        else {
+            int arr[3] = {1, 2, 5};
+            int i = 2, moves = 0;
+            int num = abs(a- b);
+            while(num > 0 && i >= 0) {
+                if(num >= arr[i]) {
+                    num -= arr[i]; moves++;
+                } else {
+                    i--;
+                }
+            }
+            cout << moves << endl;
+        }
+        
+    }
+}
