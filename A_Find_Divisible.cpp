0ba036c105f51b69705c1cb29d8f755d59615221
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-11-15 10:22:10 +0530

    A. Find Divisible added

---
+++ A_Find_Divisible.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc; cin >> tc;
+    while(tc--) {
+        int l, r; cin >> l >> r;
+        cout << l << " " << 2*l << endl;
+    }
+}
