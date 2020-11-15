fc0578200ac4232eca21daeea78e5369cc9b1f7e
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-11-15 17:38:26 +0530

    A. Gravity Flip added

---
+++ A_Gravity_Flip.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    vector<int> arr(n); for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    for(auto i : arr) cout << i << " ";
+}
