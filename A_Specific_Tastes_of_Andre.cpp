f144f3e0990d0d3ff2ad6f0ada6ed07eee6bc072
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-11-20 23:42:16 +0530

    A. Specific Tastes of Andre added

---
+++ A_Specific_Tastes_of_Andre.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc; cin >> tc;
+    while(tc--) {
+        int n; cin >> n;
+        for (int i = 0; i < n; i++) cout << 1 << " "; cout << endl; 
+    }
+}
