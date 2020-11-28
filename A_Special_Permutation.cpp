022ce8c3ecff2551c2d478f965641a70872f8b3c
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-11-28 11:51:24 +0530

    a special permutation added

---
+++ A_Special_Permutation.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int tc; cin >> tc;
+    while(tc--) {
+        int n; cin >> n;
+        for (int i = 1; i < n; i++) cout << i+1 << " "; cout << 1 << endl; 
+    }
+}
